e1550a4d5010aba0db3d2e63ea3902393e8b43ed
funkyd3v <funkyd3v@gmail.com>
2025-10-15 22:12:02 +0600

    landing page fixed

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Services\MainPage\ImageUploader;
+use App\Services\LandingPage\ImageUploader;
 use Illuminate\Http\Request;
 use App\Models\LandingPageImage;
 use Illuminate\Support\Facades\DB;
@@ -25,55 +25,6 @@ class LandingPageController extends Controller
 
     public function uploadImage(Request $request)
     {
-        $request->validate([
-            'url' => 'required|string|max:255',
-        ]);
-        
-        DB::beginTransaction();
-        try {
-            $product = LandingPageImage::firstOrCreate([
-                'url' => $request->url
-            ]);
-
-            if ($request->hasFile('main_image')) {
-                // remove previous main image
-                $product->clearMediaCollection('main_image');
-
-                $product->addMedia($request->file('main_image'))
-                        ->usingName('Main Image')
-                        ->withCustomProperties(['slot' => 'main_image'])
-                        ->toMediaCollection('main_image');
-            }
-
-            for ($i = 1; $i <= 10; $i++) {
-                $inputName = "image{$i}";
-
-                if ($request->hasFile($inputName)) {
-                    // Find existing image for that slot
-                    $oldMedia = $product->getMedia('side_images')
-                                        ->where('custom_properties.slot', $inputName)
-                                        ->first();
-
-                    // Delete old slot image if found
-                    if ($oldMedia) {
-                        $oldMedia->delete();
-                    }
-
-                    // Add new image for this slot
-                    $product->addMedia($request->file($inputName))
-                            ->usingName("Image {$i}")
-                            ->withCustomProperties(['slot' => $inputName])
-                            ->toMediaCollection('side_images');
-                }
-            }
-
-            DB::commit();
-            return redirect()->back()->with('success', 'Images updated successfully.');
-
-        } catch (\Throwable $e) {
-            DB::rollBack();
-            report($e);
-            return back()->withErrors('Upload failed: ' . $e->getMessage());
-        }
+        return $this->imageUploader->upload($request);
     }
 }
